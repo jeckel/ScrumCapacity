cf28c60fef09f37f8979e7c4d3326212709ac7bf
Julien <julien@tvty.tv>
2017-01-13 18:34:11 +0100

    Fix structure

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,19 +1,19 @@
-CREATE TABLE sprint
+CREATE TABLE `sprint`
 (
-  sprint_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name VARCHAR(64) NOT NULL,
-  nb_days INT(11),
-  created_at DATETIME NOT NULL,
-  updated_at DATETIME
-);
+  `sprint_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nb_days` INT(11),
+  `created_at` TIMESTAMP,
+  `updated_at` TIMESTAMP,
+  PRIMARY KEY (`sprint_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE sprint_member
+CREATE TABLE `sprint_member`
 (
-  sprint_member_id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  sprint_id INT(11) NOT NULL,
-  nb_days_presence INT(11),
-  availability INT(11),
-  CONSTRAINT sprint_member_sprint_sprint_id_fk FOREIGN KEY (sprint_id) REFERENCES sprint (sprint_id)
-);
-
-CREATE INDEX sprint_member_sprint_sprint_id_fk ON sprint_member (sprint_id);
+  `sprint_member_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `sprint_id` INT(11) NOT NULL,
+  `nb_days_presence` INT(11),
+  `availability` INT(11),
+  PRIMARY KEY (`sprint_member_id`),
+  KEY `sprint_id` (`sprint_id`),
+  CONSTRAINT `sprint_id_fk` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`sprint_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
